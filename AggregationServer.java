0ff00ad2e1c8be9ab5d061d649241dbfd3168000
Sascha <sascha.w@live.com>
2023-08-29 20:05:04 +0930

    added get client, reworked server socket stuff

--- AggregationServer.java
+++ AggregationServer.java
@@ -6,44 +6,41 @@ public class AggregationServer {
     public static void main(String[] args) {
         int port = 8080;
 
-        ServerSocket serverSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(port);
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[1]);
+        }
+ 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
             System.out.println("Server is listening on port " + port);
-
+ 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
-
+                Socket socket = serverSocket.accept();
+ 
+                System.out.println("New client connected");
+ 
                 // Create input and output streams for communication
-                InputStream clientInputStream = clientSocket.getInputStream();
-                InputStreamReader clientinputStreamReader = new InputStreamReader(clientInputStream);
-                BufferedReader clientInputReader = new BufferedReader(clientinputStreamReader);
+                InputStream inputStream = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                PrintWriter clientOutputWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                OutputStream outputStream = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(outputStream, true);
 
-                // Read the incoming request
-                String request = clientInputReader.readLine();
+                String request = reader.readLine();
                 System.out.println("Received request: " + request);
 
                 // Process the request (in this case, just sending a basic response)
                 String response = "HTTP/1.1 200 OK\r\n\r\nHello, this is a basic server!";
-                clientOutputWriter.println(response);
+                writer.println(response);
 
-                // Close the streams and socket
-                clientInputReader.close();
-                clientOutputWriter.close();
-                clientSocket.close();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (UnknownHostException ex) {
+ 
+            System.out.println("Server not found: " + ex.getMessage());
+ 
+        } catch (IOException ex) {
+ 
+            System.out.println("I/O error: " + ex.getMessage());
         }
     }
 }
